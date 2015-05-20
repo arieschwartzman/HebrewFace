f23778ab29bda0713bc4b239bf9032b8d8718680
Arie Schwartzman <arie@jacada.com>
2015-05-20 08:50:01 +0300

    Initial commit

---
+++ wear/src/main/java/com/schwartzman/arie/hebrewface/HebrewWatchFaceWearableConfigActivity.java
@@ -0,0 +1,9 @@
+package com.schwartzman.arie.hebrewface;
+
+import android.app.Activity;
+
+/**
+ * Created by arie on 05/05/15.
+ */
+public class HebrewWatchFaceWearableConfigActivity extends Activity {
+}
